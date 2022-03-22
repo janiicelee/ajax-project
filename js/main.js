b553df56fbc8bdeaf579bd8b3f00bac2b2bbdade
Janice Lee <meekukin@gmail.com>
2022-03-22 12:00:47 -0700

    Modified variable keywords in ES6

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* global data */
-var $susuwatari = document.querySelector('#susuwatari');
-var $frontPage = document.querySelector('.front-page');
-var $listPage = document.querySelector('#list-page');
-var $infoPage = document.querySelector('#info-page');
-var $reviewPage = document.querySelector('#review-page');
+const $susuwatari = document.querySelector('#susuwatari');
+const $frontPage = document.querySelector('.front-page');
+const $listPage = document.querySelector('#list-page');
+const $infoPage = document.querySelector('#info-page');
+const $reviewPage = document.querySelector('#review-page');
 
 // loading spinner
-var loadingSpinner = document.querySelector('.lds-default');
+const loadingSpinner = document.querySelector('.lds-default');
 loadingSpinner.className = 'lds-default';
 
 // click the susuwatari to start
@@ -17,9 +17,9 @@ function clickSuswatari(event) {
 $susuwatari.addEventListener('click', clickSuswatari);
 
 // show movie list on list page
-var $movielist = document.querySelector('#movie-list');
+const $movielist = document.querySelector('#movie-list');
 
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://ghibliapi.herokuapp.com/films');
 xhr.responseType = 'json';
 
@@ -32,8 +32,8 @@ xhr.addEventListener('load', function () {
 
   data.films = xhr.response;
 
-  for (var i = 0; i < data.films.length; i++) {
-    var $imgElement = document.createElement('img');
+  for (let i = 0; i < data.films.length; i++) {
+    const $imgElement = document.createElement('img');
     $movielist.appendChild($imgElement);
     $imgElement.setAttribute('src', xhr.response[i].image);
     $imgElement.setAttribute('data-movie-index', i);
@@ -46,7 +46,7 @@ xhr.addEventListener('load', function () {
 
 xhr.send();
 
-var $movieinfo = document.querySelector('#movie-info');
+const $movieinfo = document.querySelector('#movie-info');
 
 function handlePosterClick(event) {
 
@@ -55,8 +55,8 @@ function handlePosterClick(event) {
     $infoPage.className = '';
     $reviewPage.className = 'hidden';
 
-    var id = event.target.getAttribute('data-movie-index');
-    var movieInfoDOMTree = createMovieInfo(data.films[id]);
+    const id = event.target.getAttribute('data-movie-index');
+    const movieInfoDOMTree = createMovieInfo(data.films[id]);
     $movieinfo.appendChild(movieInfoDOMTree);
   }
 }
@@ -71,41 +71,41 @@ function removeChildNodes(parent) {
 // create info page for each movie
 function createMovieInfo(movie) {
 
-  var $movieInfoContainer = document.createElement('div');
-  var titleElement = document.createElement('h3');
+  const $movieInfoContainer = document.createElement('div');
+  const titleElement = document.createElement('h3');
   titleElement.setAttribute('id', 'movie-title');
   titleElement.textContent = movie.title + ' ' + '(' + movie.original_title + ')';
   $movieInfoContainer.appendChild(titleElement);
 
-  var imgElement = document.createElement('img');
+  const imgElement = document.createElement('img');
   imgElement.setAttribute('src', movie.movie_banner);
   imgElement.setAttribute('id', 'movie-banner');
   $movieInfoContainer.appendChild(imgElement);
 
-  var pElement = document.createElement('p');
+  const pElement = document.createElement('p');
   pElement.textContent = movie.description;
   $movieInfoContainer.appendChild(pElement);
 
-  var infobox = document.createElement('div');
+  const infobox = document.createElement('div');
   infobox.setAttribute('class', 'info-box');
   $movieInfoContainer.appendChild(infobox);
 
-  var ulElement = document.createElement('ul');
+  const ulElement = document.createElement('ul');
   infobox.appendChild(ulElement);
 
-  var liElement1 = document.createElement('li');
+  const liElement1 = document.createElement('li');
   liElement1.textContent = 'Director: ' + movie.director;
   ulElement.appendChild(liElement1);
 
-  var liElement2 = document.createElement('li');
+  const liElement2 = document.createElement('li');
   liElement2.textContent = 'Producer: ' + movie.producer;
   ulElement.appendChild(liElement2);
 
-  var liElement3 = document.createElement('li');
+  const liElement3 = document.createElement('li');
   liElement3.textContent = 'Release Year: ' + movie.release_date;
   ulElement.appendChild(liElement3);
 
-  var liElement4 = document.createElement('li');
+  const liElement4 = document.createElement('li');
   liElement4.textContent = 'Running Time: ' + movie.running_time + ' min';
   ulElement.appendChild(liElement4);
 
@@ -113,7 +113,7 @@ function createMovieInfo(movie) {
 }
 
 // click event for the 'go back' button in the info page.
-var $goBackButton = document.querySelector('#go-back');
+const $goBackButton = document.querySelector('#go-back');
 $goBackButton.addEventListener('click', goBackToList);
 
 function goBackToList(event) {
@@ -127,7 +127,7 @@ function goBackToList(event) {
 }
 
 // show list when user clicks the 'my ghibli' tab
-var $myGhibliTab = document.querySelector('#my-ghibli-tab');
+const $myGhibliTab = document.querySelector('#my-ghibli-tab');
 $myGhibliTab.addEventListener('click', function (event) {
   $movielist.className = '';
   $infoPage.className = 'hidden';
@@ -138,8 +138,8 @@ $myGhibliTab.addEventListener('click', function (event) {
   }
 });
 
-var $reviewsTab = document.querySelector('#reviews-tab');
-var $reviewsList = document.querySelector('#reviews-list');
+const $reviewsTab = document.querySelector('#reviews-tab');
+const $reviewsList = document.querySelector('#reviews-list');
 
 // show reviews when user clicks the 'reviews' tab
 $reviewsTab.addEventListener('click', clickReviewTab);
@@ -170,9 +170,9 @@ function clickReviewTab(event) {
 }
 
 // click event function for the 'add review' button
-var noReviews = document.querySelector('#no-reviews');
-var newReview = document.querySelector('#new-review');
-var $addReviewButton = document.querySelector('#add-review');
+const noReviews = document.querySelector('#no-reviews');
+const newReview = document.querySelector('#new-review');
+const $addReviewButton = document.querySelector('#add-review');
 
 function handleReviewButton(event) {
   $reviewPage.className = '';
@@ -185,21 +185,21 @@ function handleReviewButton(event) {
   // hide the delete button when adding a new review
   $deleteButton.className = 'visibility-hidden';
 
-  var reviewMovieTitle = document.querySelector('#review-movie');
-  var reviewImage = document.querySelector('#review-image');
+  const reviewMovieTitle = document.querySelector('#review-movie');
+  const reviewImage = document.querySelector('#review-image');
   reviewMovieTitle.textContent = document.querySelector('#movie-title').textContent;
   reviewImage.setAttribute('src', document.querySelector('#movie-banner').src);
 }
 
 // submit event for the review form
-var $form = document.querySelector('form');
-var $ulElement = document.querySelector('ul');
+const $form = document.querySelector('form');
+const $ulElement = document.querySelector('ul');
 
 $form.addEventListener('submit', saveReview);
 
 function saveReview(event) {
   event.preventDefault();
-  var review = {};
+  let review = {};
   if (data.editing === null) {
     review = {
       title: document.querySelector('#movie-title').textContent,
@@ -221,16 +221,16 @@ function saveReview(event) {
     review.reviewId = data.editing.reviewId;
 
     // put the edited review into the reviews list
-    for (var i = 0; i < data.reviews.length; i++) {
+    for (let i = 0; i < data.reviews.length; i++) {
       if (data.reviews[i].reviewId === review.reviewId) {
         data.reviews[i] = review;
       }
     }
 
     // replace exisiting DOM element with another one
-    var reviewItems = document.querySelectorAll('.review-list-item');
-    for (i = 0; i < reviewItems.length; i++) {
-      var reviewItemId = JSON.parse(reviewItems[i].getAttribute('data-review-id'));
+    const reviewItems = document.querySelectorAll('.review-list-item');
+    for (let i = 0; i < reviewItems.length; i++) {
+      const reviewItemId = JSON.parse(reviewItems[i].getAttribute('data-review-id'));
       if (reviewItemId === data.editing.reviewId) {
         reviewItems[i].replaceWith(createReviewListItem(review));
       }
@@ -253,29 +253,29 @@ function saveReview(event) {
 
 // create DOM to display the reviews
 function createReviewListItem(review) {
-  var liElement = document.createElement('li');
+  const liElement = document.createElement('li');
   liElement.setAttribute('class', 'review-list-item');
   liElement.setAttribute('data-review-id', review.reviewId);
 
-  var titleElement = document.createElement('h3');
+  const titleElement = document.createElement('h3');
   titleElement.setAttribute('id', 'movie-title');
   titleElement.textContent = review.title;
   liElement.appendChild(titleElement);
 
-  var imageElement = document.createElement('img');
+  const imageElement = document.createElement('img');
   imageElement.setAttribute('id', 'movie-banner');
   imageElement.setAttribute('src', review.image);
   liElement.appendChild(imageElement);
 
-  var pElement = document.createElement('p');
+  const pElement = document.createElement('p');
   pElement.textContent = review.text;
   liElement.appendChild(pElement);
 
-  var divElement = document.createElement('div');
+  const divElement = document.createElement('div');
   divElement.setAttribute('class', 'align-right');
   liElement.appendChild(divElement);
 
-  var editButton = document.createElement('button');
+  const editButton = document.createElement('button');
   editButton.setAttribute('class', 'edit-button');
   editButton.setAttribute('type', 'button');
   editButton.textContent = 'Edit Review';
@@ -285,12 +285,12 @@ function createReviewListItem(review) {
 
 }
 
-var newReviewsList = document.querySelector('#new-reviews-list');
+const newReviewsList = document.querySelector('#new-reviews-list');
 
 // remain the reviews list even after refreshing the page
 document.addEventListener('DOMContentLoaded', function (event) {
-  for (var i = 0; i < data.reviews.length; i++) {
-    var result = createReviewListItem(data.reviews[i]);
+  for (let i = 0; i < data.reviews.length; i++) {
+    const result = createReviewListItem(data.reviews[i]);
     newReviewsList.appendChild(result);
   }
 });
@@ -301,11 +301,11 @@ newReviewsList.addEventListener('click', handleEditButton);
 function handleEditButton(event) {
   if (event.target.matches('.edit-button')) {
     $deleteButton.className = '';
-    var $liClosest = event.target.closest('li');
-    var $reviewId = $liClosest.getAttribute('data-review-id');
+    const $liClosest = event.target.closest('li');
+    let $reviewId = $liClosest.getAttribute('data-review-id');
 
     $reviewId = JSON.parse($reviewId);
-    for (var i = 0; i < data.reviews.length; i++) {
+    for (let i = 0; i < data.reviews.length; i++) {
       if (data.reviews[i].reviewId === $reviewId) {
         data.editing = data.reviews[i];
       }
@@ -326,31 +326,31 @@ function handleEditButton(event) {
   }
 }
 
-var $deleteButton = document.querySelector('#delete-button');
+const $deleteButton = document.querySelector('#delete-button');
 
 // show the confirmation modal
-var $modal = document.querySelector('#modal');
+const $modal = document.querySelector('#modal');
 $deleteButton.addEventListener('click', function (event) {
   $modal.className = 'dark-bg';
 });
 
 // hide the confirmation modal when the user clicks cancel
-var $cancelButton = document.querySelector('#cancel-button');
+const $cancelButton = document.querySelector('#cancel-button');
 $cancelButton.addEventListener('click', function (event) {
   $modal.className = 'dark-bg hidden';
 });
 
 // remove the review from the data model and the dom tree
-var $confirmButton = document.querySelector('#confirm-button');
+const $confirmButton = document.querySelector('#confirm-button');
 $confirmButton.addEventListener('click', function (event) {
 
-  var reviewItems = document.querySelectorAll('.review-list-item');
-  for (var i = 0; i < data.reviews.length; i++) {
+  const reviewItems = document.querySelectorAll('.review-list-item');
+  for (let i = 0; i < data.reviews.length; i++) {
     if (data.reviews[i].reviewId === data.editing.reviewId) {
       data.reviews.splice(i, 1);
     }
 
-    var reviewIdNum = JSON.parse(reviewItems[i].getAttribute('data-review-id'));
+    const reviewIdNum = JSON.parse(reviewItems[i].getAttribute('data-review-id'));
     if (reviewIdNum === data.editing.reviewId) {
       reviewItems[i].remove();
     }
